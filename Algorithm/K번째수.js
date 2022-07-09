799a495ee3fc116a5354c5830e2f6f3f85468208
Jaewoo Gwak <jaewoo010207@gmail.com>
2022-07-10 01:59:23 +0900

    정렬

--- Algorithm/K번째수.js
+++ Algorithm/K번째수.js
@@ -1,1 +1,10 @@
-
+function solution(array, commands) {
+    var answer = [];
+    commands.forEach(command => {
+        const [start, end, K] = command;
+        let slicedArray = array.slice(start - 1, end);
+        slicedArray.sort((a,b) => a - b);
+        answer.push(slicedArray[K - 1]);
+    })
+    return answer;
+}
